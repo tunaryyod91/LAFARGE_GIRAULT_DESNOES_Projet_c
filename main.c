84c15f60862f4c1b6461c3545006004644945595
paulgirault <paul.girault@efrei.net>
2025-10-20 18:19:06 +0200

    cmake reset

--- main.c
+++ main.c
@@ -1,11 +1,14 @@
 //
 // Created by Paul Girault on 20/10/2025.
 //
+#include "Partie_1.h"
+#include "Partie_1.c"
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
 
 int main(void) {
     printf("Bonjour ! Le projet compile enfin sur macOS 😄\n");
+
     return 0;
 }
